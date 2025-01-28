2519d36067af43d62d8ec85299686c60a5c681cd
Peter Inglesby <peter.inglesby@gmail.com>
2025-01-28 13:18:55 +0000

    Fix view SQL
    For boring reasons, for the SQL for a view we need to (a) include the
    project, and (b) put the project and dataset names inside curly braces.

--- openprescribing/measures/views/vw__bdz_total_adq.sql
+++ openprescribing/measures/views/vw__bdz_total_adq.sql
@@ -19,21 +19,21 @@ SELECT
         COALESCE(strnt_dnmtr_val, 1) -- divides by unit size value, or 1 if this does not exist
     ) / adq.adq AS total_adq_usage -- gets adq usage value normalised to mg
 FROM 
-    hscic.normalised_prescribing AS rx
+    {project}.{hscic}.normalised_prescribing AS rx
 INNER JOIN 
-    dmd.vmp AS vmp
+    {project}.{dmd}.vmp AS vmp
     ON CONCAT(SUBSTR(rx.bnf_code, 0, 9), SUBSTR(rx.bnf_code, -2)) = CONCAT(SUBSTR(vmp.bnf_code, 0, 9), SUBSTR(vmp.bnf_code, -2)) --joins "generic" BNF code to dm+d
 INNER JOIN 
-    dmd.vpi AS vpi
+    {project}.{dmd}.vpi AS vpi
     ON vpi.vmp = vmp.id -- joins from vmp to vpi (where strengths are stored)
 INNER JOIN 
     measures.adq_bdz AS adq --joins to bdz ADQ table
     ON SUBSTR(rx.bnf_code, 0, 9) = adq.chemical_code
 INNER JOIN
-    dmd.droute AS droute -- joins to droute to get route codes
+    {project}.{dmd}.droute AS droute -- joins to droute to get route codes
     ON vmp.id = droute.vmp
 INNER JOIN
-    dmd.route AS route -- joins to route to get route names
+    {project}.{dmd}.route AS route -- joins to route to get route names
     ON route.cd = droute.route
 WHERE 
     route.descr = 'Oral' AND -- Oral preps only
